ff3b29ba9febb1cfc6b94383e4be260e3069b94a
Alejandro Porras <axxuma@gmail.com>
2017-02-10 17:35:37 -0600

    refactor: Adds an 'EditorTab' and establishes a default editor tab

--- editorcontainer/editorcontainer.py
+++ editorcontainer/editorcontainer.py
@@ -1,20 +1,27 @@
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
 from kivy.uix.tabbedpanel import TabbedPanelItem
+from kivy.uix.tabbedpanel import TabbedPanel
 
 from editorcontainer.editor.editor import Editor
 from editorcontainer.linenumbersstrip.linenumbersstrip import LineNumbersStrip
 
-class EditorContainer(BoxLayout):
+
+class EditorContainer(TabbedPanel):   
    
-   line_numbers_strip = ObjectProperty(None)
-   editor = ObjectProperty(None)
-   tabbed_panel = ObjectProperty(None)
+   default_editor_tab = ObjectProperty(None)
    
    def build_editor_container(self, style_loader):
        
        self.style_loader = style_loader             
-       
-       self.editor.build_editor(self.style_loader.style_name, 
-                                self.style_loader)          
-       self.tabbed_panel.add_widget(TabbedPanelItem())
+             
+       self.default_editor_tab.send_style(self.style_loader)
+            
+              
+class EditorTab(TabbedPanelItem):
+
+    line_numbers_strip = ObjectProperty(None)
+    editor = ObjectProperty(None)
+    
+    def send_style(self, style_loader):
+        self.editor.build_editor(style_loader.style_name, style_loader)
