495845bd79cbb9f982fd8b8950cba83959699dfa
Alejandro Porras <axxuma@gmail.com>
2017-02-10 22:18:56 -0600

    feat: Saves files as x file, 'Save as'
    When the name of the file saved is too long the tab header doesn't resize. It
    doesn't track the file, just saves the contents of the tab with a name and it's
    content. TODO: tracking, to keep the file updated with the content in the application

--- azaharTEA.py
+++ azaharTEA.py
@@ -19,10 +19,12 @@ class Container(BoxLayout):
     
     def build_text_editor(self):
     
-        self.style_loader = StyleLoader()             
+        self.style_loader = StyleLoader('nada')             
         
         self.editor_container.build_editor_container(self.style_loader)              
-              
+           
+        # Send editorcontainer to menubar and from there propapagate it
+        self.menu_bar.propagate_editor_container(self.editor_container)
 
 class AzaharTEAApp(App):
     
