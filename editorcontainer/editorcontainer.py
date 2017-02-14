a488448054c5cbfc92e199da21a1f0d0a375ed0b
Alejandro Porras <axxuma@gmail.com>
2017-02-13 20:50:00 -0600

    feat: Add functionality to open a file directly
    By opening directly I mean as if the file pahtname was passed as a cosole argument. It also automatically
    changes the name of the tab and also the highlitghter information for this file. The application doesn't
    yet change the footer information when changing tabs.

--- editorcontainer/editorcontainer.py
+++ editorcontainer/editorcontainer.py
@@ -1,3 +1,5 @@
+import os
+
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
 from kivy.uix.tabbedpanel import TabbedPanelItem
@@ -12,27 +14,55 @@ from editorcontainer.linenumbersstrip.linenumbersstrip import LineNumbersStrip
 
 class EditorContainer(TabbedPanel):   
    
-   def __init__(self, **kwargs):
+      
+    def __init__(self, **kwargs):
        
-       super(EditorContainer, self).__init__()
+        super(EditorContainer, self).__init__()
        
-       editor = EditorTab()
-       editor.content = EditorTabContent()
+        self.default_tab_mimetype = None
+        self.default_tab_file_path = None                     
        
-       self.add_widget(editor)               
+    def add_new_tab(self, mime_type=None, tab_name=None):
        
-   def add_new_tab(self):
+        editor = EditorTab()
+        editor_content = EditorTabContent()
+        editor.content = editor_content
+        name = editor_content.editor.change_lexer(mime_type)
+        
+        editor.change_tab_name(tab_name)
        
-       editor = EditorTab()
-       editor.content = EditorTabContent()
+        self.parent.footer.change_information({'highlight_menu': name})
        
-       self.add_widget(editor)
+        self.add_widget(editor)
        
-       #TODO Change this to 'self.switch_to(editor, do_scroll=True) 
-       # when kivy 1.9.2 releases
-       self.switch_to(editor)
-            
-              
+        #TODO Change this to 'self.switch_to(editor, do_scroll=True) 
+        # when kivy 1.9.2 releases
+        self.switch_to(editor)
+    
+        return editor
+        
+    def build_default_tab(self):
+    
+        self.build_tab(self.default_tab_file_path, self.default_tab_mimetype)
+
+    def build_tab(self, file_path, mimetype):
+    
+        text = ''
+        
+        if self.default_tab_file_path is not None:
+        
+            with open(file_path) as file:
+                text = file.read()
+                
+                 
+        dir_path, file_name = os.path.split(file_path)
+        
+        editor_tab = self.add_new_tab(mimetype,
+                         file_name)
+                         
+        editor_tab.content.editor.text = text
+        
+                   
 class EditorTab(TabbedPanelHeader):
        
     def close_editor_tab(self, widget):
@@ -67,7 +97,13 @@ class EditorTab(TabbedPanelHeader):
                     switch_index = tab_list_len - 1
                      
                 parent_panel.switch_to(parent_panel.tab_list[switch_index])
-                
+
+    def change_tab_name(self, name=None):
+        
+        if name is not None:
+            self.text = name
+
+              
 class EditorTabContent(BoxLayout):
 
     line_numbers_strip = ObjectProperty(None)
