3f30d5f74f4795d2b785a2085574e008af2ebc04
Alejandro Porras <axxuma@gmail.com>
2017-02-17 23:44:48 -0600

    feat: Adds internal documentation to editorcontainer/editorcontainer.py

--- editorcontainer/editorcontainer.py
+++ editorcontainer/editorcontainer.py
@@ -1,10 +1,13 @@
 """
-editorcontainer.py
+editorcontainer
 ==================
+
+**Module**: ``editorcontainer.editorcontainer.py``
 
 Module that contains all that's necessary to create
 the file tabs
 """
+
 import os
 
 from kivy.uix.boxlayout import BoxLayout
@@ -54,8 +57,17 @@ class CodeScrollView(ScrollView):
     
     def __init__(self, **kwargs):
         """Call __init__ from super and decide wether to show the line numbers"""
+        
         super(CodeScrollView, self).__init__(**kwargs)
+        
         self.max_num_of_lines = 0
+        """Maximum number of lines actually.
+        
+        It will increase depending on the quantity of lines in the editor.
+        Generally, it increases when 'enter' is pressed (a new line is added).
+        Defaults to 0.
+        """
+        
         self.editor.bind(focus=self.on_editor_focus)
         
         parent = self.line_numbers_strip.parent
@@ -66,14 +78,15 @@ class CodeScrollView(ScrollView):
             self.editor.bind(_lines=self.on_lines_change)
 
     def on_editor_focus(self, widget, value):
-        """Bind or unbind on_keyboard depending on the focus of the editor
+        """Bind or unbind :py:meth:`.on_keyboard` depending on the focus of the editor
         
         It binds an event (with the method :py:meth:`.on_keyboard`) to the
         :py:attr:`kivy.core.window.Window.on_keyboard` attribute.
         
-        :param widget: Instance of the :py:attr:`kivy.uix.widget` on which the event ocurred. 
+        :param widget: Instance of the :py:attr:`kivy.uix.widget` on which the event ocurred (:py:attr:`.editor`). 
         :param value: Value of the property of the event (:py:attr:`.editor.focus` in this case).        
         """
+        
         if value:
             Window.bind(on_keyboard=self.on_keyboard)
         else:
@@ -90,26 +103,32 @@ class CodeScrollView(ScrollView):
         :param value: Value of the pressed key, as a :py:obj:`str`.
         :param modifiers: List of pressed modifiers (such as 'ctrl', 'alt'...)
         """
+        
         # ctrl-s to save
         if keycode == 115 and value == 's':
             if 'ctrl' in modifiers and len(modifiers) == 1:
                 self.editor.save_tab()
         
     def on_lines_change(self, widget, value):
-        """Manage event when :py:attr:`editorcontainer.editor.editor.Editor._lines` changes
+        """Manage event when :py:attr:`editor._lines` changes
         
         Requests an update on the line numbers depending on the situation.
         
-        :param widget: Instance of the :py:attr:`kivy.uix.widget` on which the event ocurred.
+        :param widget: Instance of the :py:attr:`kivy.uix.widget` on which the event ocurred (:py:attr:`.editor`).
         :param value: Value of :py:attr:`.editor._lines` after it changed.      
         """
+        
         n = len(value)
         if n > self.max_num_of_lines:
             self.update_lines_number(self.max_num_of_lines, n)
 
-    def on_show_line_numbers(self, instance, value):
-        """Manage event when :py:attr:`.show_line_numbers` changes
+    def on_show_line_numbers(self, widget, value):
+        """Manage event when :py:attr:`.show_line_numbers` changes.
+        
+        :param widget: :py:attr:`kivy.uix.widget` on which the event ocurred (this :py:class:`.CodeScrollView`).
+        :param value: Value of :py:attr:`.show_line_numbers` after the change.
         """
+        
         if value:
             max_num_l = str(self.max_num_of_lines)
             _get_extents = self.line_numbers_strip._label_cached.get_extents
@@ -121,7 +140,15 @@ class CodeScrollView(ScrollView):
             self.line_numbers_strip.width = 0
 
     def update_lines_number(self, old, new):
-
+        """Update the lines number.
+        
+        When the number of lines increases, the line numbers should be updated
+        using this method (thid method is called by :py:meth:`.on_lines_change`).
+        
+        :param old: Old number of lines
+        :param new: New number of lines
+        """
+        
         self.max_num_of_lines = new
         lines = [str(i) for i in range(old + 1, new + 1)]
         self.line_numbers_strip.text += '\n'.join(lines) + '\n'
@@ -135,17 +162,46 @@ class CodeScrollView(ScrollView):
         
         
 class EditorContainer(TabbedPanel):   
-   
+    """Used to create a tab for a new file or load a file.
+    This :py:class:`.EditorContainer` has tabs. Each tab 
+    consists of an :py:class:`.EditorTab` and a content.
+    
+    The content will be a :py:class:`.CodeScrollView`.
+    """
       
     def __init__(self, **kwargs):
        
         super(EditorContainer, self).__init__()
        
         self.default_tab_mimetype = None
-        self.default_tab_file_path = None                     
+        """Stores the mimetype (of a file) of a default tab. 
+        
+        Used to store the mimetype of a file that is opened directly with the 
+        application (from 'console'). Defaults to :py:obj:`None`.
+        """
+        
+        self.default_tab_file_path = None
+        """Stores the path (to a file) of a default tab.
+        
+        Used to store the path of a file that is opened directly with the application 
+        (from 'console'). Defaults to :py:obj:`None`.
+        """                     
        
     def add_new_tab(self, mime_type=None, tab_name=None):
-       
+        """Add a new tab.
+        
+        The contents of the added tab depend on the mime_type and 
+        tab_name. The tab is added to this :py:class:`.EditorContainer`.
+
+        The 'text' attribute of The :py:class:`editorcontainer.editor.editor.Editor` is binded to 
+        its method :py:meth:`editorcontainer.editor.editor.Editor.text_changed` if a blank new tab
+        is created.
+                
+        :param mime_type: Mimetype of the file to open (contents of the new tab). Defaults to :py:obj:`None` if no file will be opened.
+        :param tab_name: Name to put in the tab header. It's the name of the file. Defaults to :py:obj:`None`.
+        :return: The created tab, an :py:class:`.EditorTab`.
+        """
+        
         editor_tab = EditorTab()       
         
         editor_content = CodeScrollView()
@@ -177,11 +233,33 @@ class EditorContainer(TabbedPanel):
         return editor_tab
         
     def build_default_tab(self):
-    
+        """Build a default tab. It's a tab created when the application is opened.
+        
+        The default type depends on :py:attr:`.default_tab_file_path` and 
+        :py:attr:`.default_tab_mimetype`. If those attributes are :py:obj:`None`
+        a blank new tab will be created, otherwise a tab with the contents of
+        :py:attr:`.default_tab_file_path` and highlighted for :py:attr:`.default_tab_mimetype`
+        will be created.
+        """
+        
         self.build_tab(self.default_tab_file_path, self.default_tab_mimetype)
 
     def build_tab(self, file_path, mimetype):
-    
+        """Build a tab.
+        
+        The tab will have a path file_path and a mimetype mimetype.
+        The tab name is extracted from file_path, unless it's :py:obj:`None`.This
+        name is the file name, including the file extension.
+        If the parameters are :py:obj:`None`, a blank new tab is created.
+        
+        The 'text' attribute of The :py:class:`editorcontainer.editor.editor.Editor` is binded to 
+        its method :py:meth:`editorcontainer.editor.editor.Editor.text_changed` if a not blank tab
+        is created.
+        
+        :param file_path: Path of the file to open. Defaults to :py:obj:`None`.
+        :param mimetype: Mimetype of the file to open. Defaults to :py:obj:`None`.
+        """
+        
         text = ''
         dir_path = None
         file_name = None
@@ -218,12 +296,33 @@ class EditorContainer(TabbedPanel):
         editor.bind(text=editor.text_changed)
                                                       
 class EditorTab(TabbedPanelHeader):
-       
-    close_button = ObjectProperty(None)
-    close_button_string = StringProperty('x')
+    """Tab header of a tab to add to the :py:class:`.EditorContainer`.
     
-    def close_editor_tab(self, widget):
-     
+    It will display the name of the tab (which can be the name of the file
+    opened by that tab or the default, established in the related *.kv* file).
+    """
+    
+    close_button = ObjectProperty(None)
+    """An :py:class:`kivy.properties.ObjectProperty` that's a reference
+    to the button to close the tab (the button is in the :py:class:`.EditorTab`).
+    """
+    
+    close_button_string = StringProperty('x')
+    """Text to display in the :py:attr:`.close_button`. 
+    
+    It's a :py:class:`kivy.properties.StringProperty` and defaults to 'x'.
+    This attribute's value will change when a change to the text of the 
+    :py:class:`editorcontainer.editor.editor.Editor` occurs and display an
+    asterisk (*) on top of it.
+    """
+    
+    def close_editor_tab(self):
+        """Close this :py:class:`.EditorTab`.
+
+        Determines to which tab to move (if some tab is open) when this one is
+        closed.
+        """
+        
         parent_panel = self.parent.tabbed_panel
         
         # Save the position to change tabs
@@ -238,16 +337,16 @@ class EditorTab(TabbedPanelHeader):
         
         tab_list = parent_panel.tab_list
         
-        # Find out if it's possible to change to other tab
-        # and change to it
+        # Find out if it's possible to change to other tab,
+        # then change to it
         tab_list_len = len(parent_panel.tab_list) 
         if(tab_list_len > 0):
         
-            # Its not necessary if the tab wasn't the 'current_tab'            
+            # It's not necessary if the tab wasn't the 'current_tab'            
             
             if was_current:
             
-                #Calculate to position to switch to
+                #Calculate position to switch to
                 if not switch_index == tab_list_len:
                     switch_index = switch_index % tab_list_len
                 else:
@@ -256,7 +355,8 @@ class EditorTab(TabbedPanelHeader):
                 parent_panel.switch_to(parent_panel.tab_list[switch_index])
 
     def change_tab_name(self, name=None):
-        
+        """Change the name of this :py:class:`.EditorTab`. The name is what's
+        displayed in the tab header (that is, this :py:class:`.EditorTab`)."""
         if name is not None:
             self.text = name
                             
