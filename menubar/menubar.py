20eb2f343eb4a225aed1f25a147a068cc46d2a83
Alejandro Porras <axxuma@gmail.com>
2017-02-17 20:21:23 -0600

    feat: Add internal documentation to 'azaharTEA.py'

--- menubar/menubar.py
+++ menubar/menubar.py
@@ -1,3 +1,13 @@
+"""
+menubar.py
+==========
+
+Contains the MenuBar class.
+
+The MenuBar class is used to place the main menus for
+the application.
+"""
+
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
 from kivy.uix.button import Button
@@ -7,6 +17,12 @@ from menubar.menus.filemenu import SeeMenu
 
 class MenuBar(BoxLayout):
 
+    """Contains the main menus for the application.
+    
+    These menus include (more can be added or some can be removed):
+        * File Menu: To create a new file, save a file...
+        * See Menu: To select what to see (line numbers, for example)
+    """
 
     file_menu = ObjectProperty(None)
     see_menu = ObjectProperty(None)
