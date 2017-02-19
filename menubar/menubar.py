3752e8d37746a5dc7f0bcce2cc3edb2264a768b7
Alejandro Porras <axxuma@gmail.com>
2017-02-18 22:06:09 -0600

    feat: Adds internal documentation to menubar.menubar.py

--- menubar/menubar.py
+++ menubar/menubar.py
@@ -1,6 +1,8 @@
 """
-menubar.py
-==========
+menubar
+=======
+
+**Module** : ``menubar.menubar.py``
 
 Contains the MenuBar class.
 
@@ -15,8 +17,8 @@ from kivy.uix.button import Button
 from menubar.menus.filemenu import FileMenu
 from menubar.menus.filemenu import SeeMenu
 
-class MenuBar(BoxLayout):
 
+class MenuBar(BoxLayout):
     """Contains the main menus for the application.
     
     These menus include (more can be added or some can be removed):
@@ -25,10 +27,33 @@ class MenuBar(BoxLayout):
     """
 
     file_menu = ObjectProperty(None)
-    see_menu = ObjectProperty(None)
+    """A :py:class:`kivy.properties.Property` to store a reference to
+    a :py:class:`menubar.menus.filemenu.FileMenu`.
+    Menu with options for file operations.
     
-    def propagate_editor_container(self, editor_container):
+    This options include things like:
+        * New File: To open a new tab
+        * Open File
+        * Save as: To save a file with an specific name.
+        * Save: To save all changed files that were already saved.
+    """
+    
+    see_menu = ObjectProperty(None)
+    """A :py:class:`kivy.properties.Property to store a reference to
+    a :py:class:`menubar.menus.filemenu.SeeMenu`.
+    Menu with options for "see" operations.
+    
+    This options include things like:
+        * See line number: To show or hide the editor's line numbers.
+    """
         
+    def propagate_editor_container(self, editor_container):
+        """Propagate an :py:class:`editorcontainer.editorcontainer.EditorContainer`
+        to this :py:class:`.MenuBar`. 
+        
+        :param editor_container: :py:class:`editorcontainer.editorcontainer.EditorContainer` \
+        that should be propagated. This editor_container should be :py:attr:`azaharTEA.Container.editor_container`.
+        """
         self.editor_container = editor_container
         self.file_menu.propagate_editor_container(editor_container)
         self.see_menu.propagate_editor_container(editor_container)
